2e91ff1b8b2b0496b4206147e3919ba2edc8f184
Kirsten Humphreys <kirstenrhumphreys@gmail.com>
2016-08-01 15:58:52 -0400

    updated main.js

--- main.js
+++ main.js
@@ -1,42 +1,79 @@
-var gameBoard = new Array();
-var board = getElementByClass("game-board");
+//declare variables
+var gameBoard = [];
+var board = document.getElementById("game-table");
 var currentRow
+
+newGame();
 
 // function to create game board
 // 7 columns and 6 rows
 function buildGameBoard() {
-  gameBoard = new Array();
+  gameBoard = [];
   for(var r = 0; r < 6; r++){
-    gameBoard[i] = new Array();
+    gameBoard[r] = [];
     for (var c = 0; c < 7; c++) {
-      gameBoard[i].push(0);
+      gameBoard[r].push(0)
     }
-  }
-},
+  } console.log(gameBoard)
+}
 
-
-//function to make pieces fall into colunm
-
-//function to determine two players / current player
-
-//funtion to determine a win
-
-function determineVictory() {
+//place game piece on board
+function placeGamePiece(player){
 
 }
 
-//create game pieces and add to board
+//define players
 
-function placeGamePiece(player){
+function Disc(player){
+  this.player = player;
+  this.color = player == 1 ? 'red' : 'yellow';
+  this.id = id.toString();
+  id++;
+
+  this.addToScene = function(){
+    board.innerHTML += '<div id="d'+this.id+'" class="disc '+this.color+'"></div>';
+    //if(currentPlayer==2){
+
+      // document.getElementById('d'+this.id).style.left = (14+60*currentCol)+"px";
+      //dropDisc(this.id,currentPlayer);
+  //   }
+   }
+}
+
+
+//is the column occupied
+function firstFreeRow(col,player){
+  for(var i = 0; i<6; i++){
+    if(gameBoard[i][col]!=0){
+      break;
+    }
+  }
+}
+
+function cellVal(row,col){
+  if(gameField[row] == undefined || gameField[row][col] == undefined){
+    return -1;
+  } else {
+    return gameField[row][col];
+  }
+}
+//place piece at the bottom of the column
+
+//determine current player
+
+//determine a win
+function determineVictory() {
 
 }
 
 //run new game
 
-function newgame(){
+function newGame(){
   buildGameBoard();
-  placeGamePiece(Math.floor(Math.random()*2)+1);
-},
+  // placeGamePiece();
+}
 
-newGame();
+
+
+
 
