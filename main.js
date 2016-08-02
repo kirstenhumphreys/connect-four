8fc5cf1c23b13fcb3bda1f24764e42b9960b1545
Kirsten Humphreys <kirstenrhumphreys@gmail.com>
2016-08-02 11:32:41 -0400

    two player functionality

--- main.js
+++ main.js
@@ -1,111 +1,83 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-//declare variables
-var gameBoard = [];
-var board = document.getElementById("game-table");
-var currentRow;
-var currentPlayer = 1;
-$('#game-table').
-// newGame();
+    //declare variables
+    var gameBoard = [];
+    var board = document.getElementById("game-table");
+    var currentRow;
+    var currentPlayer = 1;
+    $('#game-table');
+    // newGame();
 
-// function to create game board
-// 7 columns and 6 rows
-function buildGameBoard() {
-    gameBoard = [];
-    for (var r = 0; r < 6; r++) {
-        gameBoard[r] = [];
-        for (var c = 0; c < 7; c++) {
-            gameBoard[r].push(0)
+    // function to create game board
+    // 7 columns and 6 rows
+    function buildGameBoard() {
+        gameBoard = [];
+        for (var r = 0; r < 6; r++) {
+            gameBoard[r] = [];
+            for (var c = 0; c < 7; c++) {
+                gameBoard[r].push(0)
+            }
+        }
+        console.log(gameBoard)
+        console.log(gameBoard[0][4])
+
+        //for loop bottom up
+    }
+
+    //defines the player and puts their color circle on the board, called below
+    function placeGamePiece(player, myThis) {
+        var whichPlayer = 'circlep' + currentPlayer;
+        myThis.addClass(whichPlayer);
+    }
+
+    //is the column occupied
+    function firstFreeRow(col, player) {
+        for (var i = 0; i < 6; i++) {
+            if (gameBoard[i][col] != 0) {
+                break;
+            }
         }
     }
-    console.log(gameBoard)
-    console.log(gameBoard[x][4])
 
-    //for loop bottom up
-    for (var i=6; i >=0; i--;) {
+    //place piece at the bottom of the column
+
+
+
+    //determine current player
+
+    //determine a win
+    function determineVictory() {
 
     }
-}
 
-//place game piece on board
-function gamePiece(player){
-  this.player = player;
-  this.color = player == 1 ? 'red' : 'yellow';
-  this.id = id.toString();
-  id++;
+    //run new game
 
-  this.addToScene = function(){
-    board.innerHTML += '<div id="d'+this.id+'" class="disc '+this.color+'"></div>';
-  }
-}
+    function newGame() {
+        buildGameBoard();
+        // placeGamePiece();
+    }
 
-function placeGamePiece(player) {
-currentPlayer = player;
-var gamePiece = new gamePiece(player);
-gamePiece.addToBoard();
-}
+    $('.col').click(function() {
+        console.log(this.classList[1]);
+        console.log(this.classList[2]);
+        console.log(currentPlayer);
+        //Check for present pieces
+
+        //call the fuction to place game pieces on board
+       placeGamePiece(currentPlayer, $(this));
+
+        //Drop piece for that player
+        // for (var r=6; r >=0; i--;) {
+
+        //Check for win logic
 
 
-//define players
-
-function Disc(player) {
-    this.player = player;
-    this.color = player == 1 ? 'red' : 'yellow';
-    this.id = id.toString();
-    id++;
-}
-
-//is the column occupied
-function firstFreeRow(col, player) {
-    for (var i = 0; i < 6; i++) {
-        if (gameBoard[i][col] != 0) {
-            break;
+        //if not a win then switch players
+        if (currentPlayer === 1) {
+            currentPlayer = 2;
+        } else {
+            currentPlayer = 1;
         }
-    }
-}
-
-function cellVal(row, col) {
-    if (gameField[row] == undefined || gameField[row][col] == undefined) {
-        return -1;
-    } else {
-        return gameField[row][col];
-    }
-}
-//place piece at the bottom of the column
-
-
-
-//determine current player
-
-//determine a win
-function determineVictory() {
-
-}
-
-//run new game
-
-function newGame() {
-    buildGameBoard();
-    // placeGamePiece();
-}
-
-$('.col').click(function(){
-  console.log(this);
-  console.log("CLICK");
-  console.log(currentPlayer)
-  //Check for present pieces
-  $(this).addClass("circle");
-  //Drop piece for that player
-
-  //Check for win logic
-
-
-  //if not a win then switch players
-  if (currentPlayer ===1){
-    currentPlayer = 2;
-  } else {
-    currentPlayer = 1;
-  }
-});
+    });
 
 });
